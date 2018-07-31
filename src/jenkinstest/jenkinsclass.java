c2d5bfd3f6ba07f52839aca86352409641b41217
Anbarasi Ajithkumar <anbarasi@macrosoftindia.com>
2018-07-31 16:34:04 +0530

    Project Commit

---
+++ src/jenkinstest/jenkinsclass.java
@@ -0,0 +1,31 @@
+package jenkinstest;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+//testing
+public class jenkinsclass 
+{
+	@Test
+		public void zephyrtesting()
+	{
+		System.out.println(" Welcome to my world");
+		System.setProperty("webdriver.chrome.driver","F:\\selenium-files\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String aTitle ="";
+driver.get("https://www.google.com");
+driver.manage().window().maximize();
+aTitle= driver.getTitle();
+if (aTitle.contentEquals("Google"))
+{
+	System.out.println("Test Passed");
+}
+else
+{
+	System.out.println("Test failedd");	
+}
+driver.quit();
+}
+}
